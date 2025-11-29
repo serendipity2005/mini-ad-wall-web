a94312863cbb212320a229c5ed4bca5232257a7e
serendipity <2843306836@qq.com>
2025-11-29 11:30:23 +0800

    feat：修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'mini-ad-wall',//服务器路径
   plugins: [
     tailwindcss(),
     vue(),
@@ -29,6 +30,20 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  build: {
+    // 优化打包体积：拆分 chunks，减小单个文件大小
+    chunkSizeWarningLimit: 1500, // 超过 1500KB 才报警告（默认 500KB）
+    rollupOptions: {
+      output: {
+        // 按模块拆分 chunks（便于缓存）
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
